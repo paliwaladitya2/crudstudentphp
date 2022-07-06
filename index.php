cdf282422f9f218b4fc0ed0f85b1e3baa8aa276e
paliwaladitya2 <74973366+paliwaladitya2@users.noreply.github.com>
2022-07-06 18:00:11 +0530

    index page logic changed

--- index.php
+++ index.php
@@ -1,13 +1,3 @@
-<?php
-
-	session_start();
-    if($_SESSION['loggedin']="true" || ($_SESSION['loggedin']=true)){
-       header("location: welcome.php");
-       exit;
-    }
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
